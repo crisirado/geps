bd4481aff7de2b4b080cbf144a68e94a61626a7f
Cristiano <crisirado@gmail.com>
2015-02-21 15:24:58 -0300

    21.02.2015

--- conf/postgres.sql
+++ conf/postgres.sql
@@ -1,1 +1,22 @@
+--Create Schema
+CREATE SCHEMA geps;
+
+--Criando tabela de usuarios
+
+CREATE TABLE geps.usuarios
+(
+  id serial NOT NULL,
+  nome character varying(255) NOT NULL,
+  email character varying(255) NOT NULL,
+  senha text NOT NULL,
+  CONSTRAINT usuarios_pkey PRIMARY KEY (id),
+  CONSTRAINT usuarios_email_key UNIQUE (email)
+)
+WITH (
+  OIDS=FALSE
+);
+
+--Criando usuário admin. Você pode criar um outro usuário
+INSERT INTO geps.usuarios(nome, email, senha) 
+    VALUES ( 'Administrador', 'admin@minhaempresa.com', md5('admin'));
 
