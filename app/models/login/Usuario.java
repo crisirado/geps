0e4730f305c2cb45a8315b26571860de989d8f2d
Cristiano <crisirado@gmail.com>
2015-03-09 14:39:28 -0400

    Models

--- app/models/login/Usuario.java
+++ app/models/login/Usuario.java
@@ -1,15 +1,41 @@
 package models.login;
 
+import commons.Extensions;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import play.data.validation.Email;
+import play.data.validation.Password;
 import play.db.jpa.Model;
 
+@Entity
+@Table(schema = "GEPS", name = "USUARIOS")
+public class Usuario extends Model {
 
-public class Usuario extends Model{
-
-    public String login;
+    public String nome;
+    @Password
+    @Column(name = "senha")
     public String password;
     @Email
     public String email;
-    
-    public Boolean admin = Boolean.FALSE;
+
+    //public Boolean admin = Boolean.FALSE;
+    public static Boolean verificar(String usuario, String senha) {
+        String md5Senha = null;
+        Usuario logado;
+        try {
+            md5Senha = Extensions.toMD5(senha);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        logado = Usuario.find("byEmailAndPassword", usuario, md5Senha).first();
+        if (logado != null) {
+            return Boolean.TRUE;
+        } else {
+            return Boolean.FALSE;
+        }
+    }
 }
