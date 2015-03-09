eb760057a1528822f3f719fad4b3e2b295977823
Cristiano <crisirado@gmail.com>
2015-03-09 14:21:44 -0400

    Controllers

---
+++ app/controllers/Projetos.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ *
+ * @author cristiano
+ */
+public class Projetos extends Controller{
+    
+    public static void index(){
+        render();
+    }
+    
+}
---
+++ app/controllers/Usuarios.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Usuarios extends Controller {
+
+    public static void index() {
+        render();
+    }
+}
