f2c009e4c0e89a7262a3b0e04631ec03b3a63449
Cristiano <crisirado@gmail.com>
2015-03-17 21:57:54 -0400

    Controller

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -1,10 +1,22 @@
 package controllers;
 
+import java.util.List;
+import models.login.Usuario;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 
 public class Usuarios extends Controller {
-
+    @Transactional(readOnly = true)
     public static void index() {
-        render();
+        List<Usuario> usuarios = Usuario.findAll();
+
+        render(usuarios);
+    }
+    
+    @Transactional(readOnly = true)
+    public static void visualizar(Integer usuarioId){
+        Usuario usuario = Usuario.findById(Long.valueOf(usuarioId.toString()));
+        
+        render(usuario);
     }
 }
